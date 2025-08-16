265ce0dbf43532cd76408b73eb64635c5c422cdb
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2025-08-16 00:56:41 -0400

    switching to latent gaussian

--- train.py
+++ train.py
@@ -73,7 +73,7 @@ class GSSTrainer(Trainer):
 
         rgb = self.data['rgb'][ind].detach().cpu().numpy()
         out = self.gaussRender(pc=self.model, camera=camera)
-        rgb_pd = out['latent'].detach().cpu().numpy()[..., :3]
+        rgb_pd = out['render'].detach().cpu().numpy()[..., :3]
         depth_pd = out['depth'].detach().cpu().numpy()[..., 0]
         depth = self.data['depth'][ind].detach().cpu().numpy()
         # if the shape does not match, resize the depth_pd
