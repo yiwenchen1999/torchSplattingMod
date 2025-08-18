6bff6b9502919e5e11de099c650d64d96b1bfdbc
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2025-08-18 10:40:50 -0400

    depth processing

--- scripts/encode_latents.sh
+++ scripts/encode_latents.sh
@@ -1,5 +1,5 @@
 cd ../data-preprocess
 python encode_with_vae.py \
---input ../B075X65R3X \
---output ../B075X65R3X/vae_latents
+--input ../../nerf_synthetic/ship_latents_processed \
+--output ../../nerf_synthetic/ship_latents_processed/vae_latents
 
