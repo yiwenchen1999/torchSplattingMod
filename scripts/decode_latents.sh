1f5efbf4655cee3749133b55306863dc31f0a168
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2025-08-21 07:52:32 -0400

    add grad for opacity and cyz

--- scripts/decode_latents.sh
+++ scripts/decode_latents.sh
@@ -1,5 +1,5 @@
 cd ../data-preprocess
 
 python decode_with_vae.py \
---input ../result/ship_2 \
---output ../result/ship_2_decoded
+--input ../result/ship_lr1e-3 \
+--output ../result/ship_lr1e-3_decoded
