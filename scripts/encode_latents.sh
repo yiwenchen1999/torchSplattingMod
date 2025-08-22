36558ea6c663a2896310bd2eac601baec4956fa3
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2025-08-22 17:37:44 -0400

    latent ship model

--- scripts/encode_latents.sh
+++ scripts/encode_latents.sh
@@ -1,5 +1,5 @@
 cd ../data-preprocess
 python encode_with_vae.py \
---input ../../nerf_synthetic/ship_latents_processed \
---output ../../nerf_synthetic/ship_latents_processed/vae_latents
+--input ../../nerf_synthetic/ship_latents_processed_test \
+--output ../../nerf_synthetic/ship_latents_processed_test/vae_latents
 
