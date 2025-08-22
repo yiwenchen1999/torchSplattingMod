148d84bf5151ba6aafc9fc8fc4f248491c05d743
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2025-08-22 17:45:19 -0400

    latent ship model

--- gaussian_splatting/utils/data_utils.py
+++ gaussian_splatting/utils/data_utils.py
@@ -51,8 +51,9 @@ def read_all(folder, resize_factor=1., latent_model=False):
         src_rgb , src_depth, src_alpha, src_camera = \
         read_image(src_rgb_file, src_pose, 
             intrinsic, max_depth=max_depth, resize_factor=resize_factor)
-        latent_file = src_rgb_file.replace('ship_latents_processed','ship_latents_processed/vae_latents')
-        latent_file = latent_file.replace('png','npy')
+        file_name = src_rgb_file.split('/')[-1]
+        latent_file = os.path.join(folder, 'vae_latents', file_name.replace('png','npy'))
+        # latent_file = latent_file.replace('png','npy')
         if latent_model:
             src_latent = torch.from_numpy(np.load(latent_file))
             src_latent = src_latent.permute(1,2,0)
