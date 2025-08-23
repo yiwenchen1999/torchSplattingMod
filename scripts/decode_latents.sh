c1b8608f44f01a8d0014b48d065a709355850f51
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2025-08-23 16:32:55 -0400

    command for decoding

--- scripts/decode_latents.sh
+++ scripts/decode_latents.sh
@@ -5,5 +5,9 @@ cd ../data-preprocess
 # --output ../../nerf_synthetic/ship_latents_processed_test/vae_latents_decoded
 
 python decode_with_vae.py \
---input ../result/ship_latents \
---output ../result/ship_latents_decoded
+--input ../result/ship_latents_64_adjustedInit \
+--output ../result/ship_latents_64_decoded_adjustedInit
+
+python decode_with_vae.py \
+--input ../result/ship_latents_128_adjustedInit \
+--output ../result/ship_latents_128_decoded_adjustedInit
