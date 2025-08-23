7e402d1aa43c76ccc596f55f69d58261ef20d037
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2025-08-22 22:12:59 -0400

    decode latents for visualization

--- data-preprocess/decode_with_vae.py
+++ data-preprocess/decode_with_vae.py
@@ -88,7 +88,8 @@ def main():
         # Load latent
         # print('lat_path', lat_path)
         arr = np.load(lat_path, allow_pickle=True)  # (4,h,w)
-        print('arr', arr.shape, arr.dtype)
+        if arr.dtype == object:
+            arr = np.array(arr.tolist(), dtype=np.float32)  # convert to numeric
         lat = torch.from_numpy(arr).to(args.device, dtype=dtype)
         if lat.dim() == 3:
             lat = lat.unsqueeze(0)  # (1,4,h,w)
