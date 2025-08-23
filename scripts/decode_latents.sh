9bfed69c4c995834885c7864bf7309bb98676286
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2025-08-22 22:11:37 -0400

    decode latents for visualization

--- scripts/decode_latents.sh
+++ scripts/decode_latents.sh
@@ -5,5 +5,5 @@ cd ../data-preprocess
 # --output ../../nerf_synthetic/ship_latents_processed_test/vae_latents_decoded
 
 python decode_with_vae.py \
---input ../result/ship_latents \
---output ../result/ship_latents_decoded
+--input ../result/ship_latents_2048 \
+--output ../result/ship_latents_2048_decoded
