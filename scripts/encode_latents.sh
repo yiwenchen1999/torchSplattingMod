ec3f4be5367d9726a75370dce7fbc886f2500f3c
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2025-08-23 10:19:37 -0400

    latent render of size 64

--- scripts/encode_latents.sh
+++ scripts/encode_latents.sh
@@ -1,7 +1,7 @@
 cd ../data-preprocess
 python encode_with_vae.py \
 --input ../../nerf_synthetic/ship_latents_processed_test \
---output ../../nerf_synthetic/ship_latents_processed_test/vae_latents_256 \
+--output ../../nerf_synthetic/ship_latents_processed_test/vae_latents_64 \
 --size 512
 
 cd ..
