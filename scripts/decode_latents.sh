ec12d9187b637701599105877980007937677ea7
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2025-09-02 12:16:27 -0400

    objaverse exp

--- scripts/decode_latents.sh
+++ scripts/decode_latents.sh
@@ -5,9 +5,9 @@ cd ../data-preprocess
 # --output ../../nerf_synthetic/ship_latents_processed_test/vae_latents_decoded
 
 python decode_with_vae.py \
---input ../result/ship_latents_64_adjustedInit \
---output ../result/ship_latents_64_decoded_adjustedInit
+--input ../result/ship_latents_64/eval_step_90000 \
+--output ../result/ship_latents_64/eval_step_90000_decoded
 
-python decode_with_vae.py \
---input ../result/ship_latents_128_adjustedInit \
---output ../result/ship_latents_128_decoded_adjustedInit
+# python decode_with_vae.py \
+# --input ../result/ship_latents_128_adjustedInit \
+# --output ../result/ship_latents_128_decoded_adjustedInit
