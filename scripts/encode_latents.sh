d7537b7129556b9ada4432d0d07c01b25ff7e4ef
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2025-09-02 11:06:49 -0400

    objaverse exp

--- scripts/encode_latents.sh
+++ scripts/encode_latents.sh
@@ -1,45 +1,34 @@
 cd ../data-preprocess
 target=atlus
 python encode_with_vae.py \
---input ../../objaverse_synthetic/${target}_processed_test \
---output ../../objaverse_synthetic/${target}_processed_test/vae_latents_64 \
+--input ../../objaverse_synthetic/${target}_processed_train \
+--output ../../objaverse_synthetic/${target}_processed_train/vae_latents_64 \
 --size 512
 
 python encode_with_vae.py \
---input ../../objaverse_synthetic/${target}_processed_test \
---output ../../objaverse_synthetic/${target}_processed_test/vae_latents_128 \
+--input ../../objaverse_synthetic/${target}_processed_train \
+--output ../../objaverse_synthetic/${target}_processed_train/vae_latents_128 \
 --size 1024
 
 target=shop
 python encode_with_vae.py \
---input ../../objaverse_synthetic/${target}_processed_test \
---output ../../objaverse_synthetic/${target}_processed_test/vae_latents_64 \
+--input ../../objaverse_synthetic/${target}_processed_train \
+--output ../../objaverse_synthetic/${target}_processed_train/vae_latents_64 \
 --size 512
 
 python encode_with_vae.py \
---input ../../objaverse_synthetic/${target}_processed_test \
---output ../../objaverse_synthetic/${target}_processed_test/vae_latents_128 \
+--input ../../objaverse_synthetic/${target}_processed_train \
+--output ../../objaverse_synthetic/${target}_processed_train/vae_latents_128 \
 --size 1024
 
 target=statue
 python encode_with_vae.py \
---input ../../objaverse_synthetic/${target}_processed_test \
---output ../../objaverse_synthetic/${target}_processed_test/vae_latents_64 \
+--input ../../objaverse_synthetic/${target}_processed_train \
+--output ../../objaverse_synthetic/${target}_processed_train/vae_latents_64 \
 --size 512
 
 python encode_with_vae.py \
---input ../../objaverse_synthetic/${target}_processed_test \
---output ../../objaverse_synthetic/${target}_processed_test/vae_latents_128 \
+--input ../../objaverse_synthetic/${target}_processed_train \
+--output ../../objaverse_synthetic/${target}_processed_train/vae_latents_128 \
 --size 1024
 
-target=cheeseplate
-
-python encode_with_vae.py \
---input ../../objaverse_synthetic/${target}_processed_test \
---output ../../objaverse_synthetic/${target}_processed_test/vae_latents_64 \
---size 512
-
-python encode_with_vae.py \
---input ../../objaverse_synthetic/${target}_processed_test \
---output ../../objaverse_synthetic/${target}_processed_test/vae_latents_128 \
---size 1024
