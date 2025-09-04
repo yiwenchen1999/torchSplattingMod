cbf753a807f1547c86830214fc41a375f96ad410
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2025-09-03 22:43:55 -0400

    full ppl

---
+++ scripts/full_ppl_g0.sh
@@ -0,0 +1,51 @@
+cd ../data-preprocess
+targets=("statue" "bus" "houseA" "houseB" "shop" "atlus" "chair" "lizard" "lamp" "apron" "sofa" "cake" "cheeseplate" "houseB")
+
+for target in "${targets[@]}"; do
+    echo "Processing target: $target"
+    
+    python encode_with_vae.py \
+    --input ../../objaverse_synthetic/${target}_processed_train \
+    --output ../../objaverse_synthetic/${target}_processed_train/vae_latents_64 \
+    --size 512
+    cd ..
+    python train_latents.py \
+    --folder ../../objaverse_synthetic \
+    --scene_name ${target} \
+    --image_size 64
+    cd data-preprocess
+    python decode_latents.py \
+    --input ../result/${target}_64/eval_step_150000/ \
+    --output ../decoded_result/${target}_64/eval_step_150000_decoded
+
+    python encode_with_vae.py \
+    --input ../../objaverse_synthetic/${target}_processed_train \
+    --output ../../objaverse_synthetic/${target}_processed_train/vae_latents_128 \
+    --size 1024
+    python train_latents.py \
+    --folder ../../objaverse_synthetic \
+    --scene_name ${target} \
+    --image_size 128
+    cd data-preprocess
+    python decode_latents.py \
+    --input ../result/${target}_128/eval_step_150000/ \
+    --output ../decoded_result/${target}_128/eval_step_150000_decoded
+
+    python encode_with_vae.py \
+    --input ../../objaverse_synthetic/${target}_processed_train \
+    --output ../../objaverse_synthetic/${target}_processed_train/vae_latents_256 \
+    --size 2048
+    python train_latents.py \
+    --folder ../../objaverse_synthetic \
+    --scene_name ${target} \
+    --image_size 256
+    cd data-preprocess
+    python decode_latents.py \
+    --input ../result/${target}_256/eval_step_150000/ \
+    --output ../decoded_result/${target}_256/eval_step_150000_decoded
+
+    
+    echo "Completed processing for target: $target"
+    echo "----------------------------------------"
+done
+
