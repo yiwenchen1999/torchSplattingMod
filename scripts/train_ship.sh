06e19e178b41895bbd989ca7e3d042f44d54ad91
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2025-09-04 20:36:12 -0400

    full ppl added shiplatents accomodation

--- scripts/train_ship.sh
+++ scripts/train_ship.sh
@@ -1,6 +1,6 @@
 cd ..
 python train_latents.py \
---folder ../nerf_synthetic/ship_latents \
+--folder ../nerf_synthetic/ship_latents_processed_test \
 --scene_name ship_latents_128_fullEval \
 --image_size 128
 
