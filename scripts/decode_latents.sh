f10600f6d83a0fc8e5b820821b853ed706a3a409
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2025-09-08 13:33:42 -0400

    fix ppl 2

--- scripts/decode_latents.sh
+++ scripts/decode_latents.sh
@@ -5,18 +5,22 @@ cd ../data-preprocess
 # --output ../../nerf_synthetic/ship_latents_processed_test/vae_latents_decoded
 
 python decode_with_vae.py \
---input ../result/ship_latents_128_fullEval/eval_step_100000/ \
---output ../decoded_result/ship_latents_128_fullEval/eval_step_100000_decoded
-
-python decode_with_vae.py \
---input ../result/ship_latents_64_fullEval/eval_step_100000/ \
---output ../decoded_result/ship_latents_64_fullEval/eval_step_100000_decoded
-
-python decode_with_vae.py \
---input ../../nerf_synthetic/ship_latents_processed_test/vae_latents_128 \
---output ../decoded_result/ship_latents_processed_test/vae_latents_128_decoded
+--input ../result/ship_latents_fullEval_128/eval_step_100000/ \
+--output ../decoded_result/ship_latents_fullEval_128/eval_step_100000_decoded
 
 # python decode_with_vae.py \
+# --input ../result/ship_latents_64_fullEval/eval_step_100000/ \
+# --output ../decoded_result/ship_latents_64_fullEval/eval_step_100000_decoded
+
+# python decode_with_vae.py \
+# --input ../../nerf_synthetic/ship_latents_processed_test/vae_latents_128 \
+# --output ../decoded_result/ship_latents_processed_test/vae_latents_128_decoded
+
+# python decode_with_vae.py \
+# --input ../../nerf_synthetic/ship_latents_processed_test/vae_latents_64 \
+# --output ../decoded_result/ship_latents_processed_test/vae_latents_64_decoded
+
+ß# python decode_with_vae.py \
 # --input ../result/statue_64_fullEval/eval_step_100000/ \
 # --output ../result/statue_64_fullEval/eval_step_100000_decoded
 
