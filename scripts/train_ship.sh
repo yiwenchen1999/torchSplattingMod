59ba20a39dfc9833c16b3a9c43001d9fd2f990a5
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2025-09-11 16:22:45 -0400

    constructing volume for flux latents

--- scripts/train_ship.sh
+++ scripts/train_ship.sh
@@ -1,10 +1,11 @@
 cd ..
 python train_latents.py \
 --input_folder ../nerf_synthetic/ship_latents_processed_test \
---scene_name ship_latents_fullEval \
---image_size 128
+--scene_name ship_latents_flux64 \
+--image_size 64
 
 cd ../data-preprocess
 python decode_with_vae.py \
---input ../result/ship_latents_fullEval_128/eval_step_150000/ \
---output ../decoded_result/ship_latents_fullEval_128/eval_step_150000_decoded
+--input ../result/ship_latents_flux64_64/eval_step_100000/ \
+--output ../decoded_result/ship_latents_flux64_64/eval_step_100000_decoded
+
