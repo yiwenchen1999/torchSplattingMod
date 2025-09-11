8ddc39b2ae4699bd739078a41e4935844b019b89
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2025-09-11 16:27:42 -0400

    constructing volume for flux latents

--- scripts/train_ship.sh
+++ scripts/train_ship.sh
@@ -2,7 +2,7 @@ cd ..
 python train_latents.py \
 --input_folder ../nerf_synthetic/ship_latents_processed_test \
 --scene_name ship_latents_flux64 \
---image_size 64
+--image_size 64 \
 --feature_dim 16
 
 cd ../data-preprocess
