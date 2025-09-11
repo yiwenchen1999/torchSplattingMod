54b39dc7a769603fd352161031e54e44226a05e3
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2025-09-11 11:59:07 -0400

    testing script for FLUX VAE

--- scripts/decode_latents.sh
+++ scripts/decode_latents.sh
@@ -1,8 +1,8 @@
 cd ../data-preprocess
 
 python decode_with_vae.py \
-    --input ../../nerf_synthetic/ship_latents_processed_test/vae_latents \
-    --output ../../nerf_synthetic/ship_latents_processed_test/vae_latents_decoded_with_flux \
+    --input ../../nerf_synthetic/ship_latents_processed_test/vae_latents_64 \
+    --output ../../nerf_synthetic/ship_latents_processed_test/vae_latents_64_decoded_with_flux \
     --flux black-forest-labs/FLUX.1-dev \
     --device cuda \
     --fp16
