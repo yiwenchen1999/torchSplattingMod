241269e6f59ad7c640682a4bc5fe2c93ed63dba4
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2025-09-11 20:19:11 -0400

    constructing volume for flux latents

--- scripts/train_ship.sh
+++ scripts/train_ship.sh
@@ -1,12 +1,18 @@
 cd ..
 python train_latents.py \
 --input_folder ../nerf_synthetic/ship_latents_processed_test \
---scene_name ship_latents_flux64 \
+--scene_name ship_latents_flux64_init128 \
 --image_size 64 \
 --feature_dim 16
 
-cd ../data-preprocess
-python decode_with_vae.py \
---input ../result/ship_latents_flux64_64/eval_step_100000/ \
---output ../decoded_result/ship_latents_flux64_64/eval_step_100000_decoded
+# cd ../data-preprocess
+# python decode_with_vae.py \
+# --input ../result/ship_latents_flux64_64/eval_step_100000/ \
+# --output ../decoded_result/ship_latents_flux64_64/eval_step_100000_decoded
 
+# python decode_with_flux.py \
+#     --input ../result/ship_latents_flux64_64/eval_step_100000/ \
+#     --output ../decoded_result/ship_latents_flux64_64/eval_step_100000_decoded \
+#     --flux black-forest-labs/FLUX.1-dev \
+#     --device cuda \
+#     --fp16
